e3842bdbc67fa4894ceac2f51d68ce77a9a109e8
Luciano <lucholeyria@gmail.com>
2021-07-05 13:42:01 -0300

    Se implemento la funcionalidad para calcular el saldo de las cuentas

--- controllers/cuentaControllers.js
+++ controllers/cuentaControllers.js
@@ -8,7 +8,34 @@ exports.getCuentas = async (req, res) => {
          where: {
             estado: true,
          },
+         select: {
+            id: true,
+            fecha_creacion: true,
+            nombre: true,
+            saldo_inicial: true,
+            estado: true,
+            movimientos: {
+               select: {
+                  id: true,
+                  fecha: true,
+                  nombre: true,
+                  monto: true,
+                  categoria: {
+                     select: {
+                        nombre: true,
+                     },
+                  },
+                  tipo_movimiento: {
+                     select: {
+                        nombre: true,
+                     },
+                  },
+               },
+            },
+         },
       });
+
+      calcularSaldo(cuentas);
 
       res.status(200).json({
          status: "OK",
@@ -69,3 +96,25 @@ exports.deleteCuenta = async (req, res) => {
       console.log(e);
    }
 };
+
+function calcularSaldo(cuentas) {
+   cuentas.forEach((cuenta) => {
+      let ingresos = [];
+      let gastos = [];
+
+      if (cuenta.saldo_inicial) ingresos.push(cuenta.saldo_inicial);
+      cuenta.movimientos.forEach((mov) => {
+         if (mov.tipo_movimiento.nombre === "gasto") {
+            gastos.push(mov.monto);
+         } else if (mov.tipo_movimiento.nombre === "ingreso") {
+            ingresos.push(mov.monto);
+         }
+      });
+
+      ingresos = ingresos.reduce((acc, curr) => acc + curr);
+      gastos = gastos.reduce((acc, curr) => acc + curr);
+
+      cuenta.saldo = ingresos - gastos;
+      delete cuenta.movimientos;
+   });
+}
